3d0e11738ee42c4f41db300f1c0670ff2f175f2c
chiuping24 <33677895+chiuping24@users.noreply.github.com>
2017-12-15 17:41:56 +0800

    Update Refactor.bat

--- clang-tidy/Refactor.bat
+++ clang-tidy/Refactor.bat
@@ -14,26 +14,26 @@ echo whether write: >> output.log
 echo %option1% >> output.log
  
 if %option1%==y (
-  ::y
-  clang --version >>output.log 
+   ::y
+   clang --version >>output.log 
 
-  ClangExample.exe %list% >> output.log
+   ClangExample.exe %list% >> output.log
 )
 
 move Rewritefilelist.txt RewriteOutput\ >> output.log
 
-  for /f "delims=" %%b in (RewriteOutput\RewriteFileCopy.txt) do (
-    set backuplist=%%b
-  )
-  %backuplist% 
-  echo if rewrited: BackUp old code ok! >> output.log
+for /f "delims=" %%b in (RewriteOutput\RewriteFileCopy.txt) do (
+   set backuplist=%%b
+)
+%backuplist% 
+echo if rewrited: BackUp old code ok! >> output.log
 
-  for /f "delims=" %%c in (RewriteOutput\RewriteFileCopy2.txt) do (
-    set copylist=%%c
-  )
-  %copylist%
-  echo need to use % copylist % , now // it 
-  echo if rewrited: Renew the files with rewriter ok! >> output.log
+for /f "delims=" %%c in (RewriteOutput\RewriteFileCopy2.txt) do (
+   set copylist=%%c
+)
+%copylist%
+echo need to use % copylist % , now // it 
+echo if rewrited: Renew the files with rewriter ok! >> output.log
 
 set /p check=Whether using Clang-tidy to check the modernize of C++ STL (transfer into C++ 11)? (y: checking, n: without checking, f: checking and fixing) 
 
@@ -41,16 +41,16 @@ echo whether clang-tidy  (y: checking, n: without checking, f: checking and fixi
 echo %check% >> output.log
 
 if %check%==y  (
-::check
-clang --version >>output.log 
-clang-tidy --version >>output.log 
-run-clang-tidy.py -checks='-*,moder*over*,read*braces*' >> output.log
-run-clang-tidy.py -checks='-*,moder*over*,read*braces*' 
+   ::check
+   clang --version >>output.log 
+   clang-tidy --version >>output.log 
+   run-clang-tidy.py -checks='-*,moder*over*,read*braces*' >> output.log
+   run-clang-tidy.py -checks='-*,moder*over*,read*braces*' 
 ) else if %check%==f  (
-::fix
-run-clang-tidy.py -checks='-*,moder*over*,read*braces*' >> output.log
-TidyBackUp.exe
-echo check and backup check-problem file 
+   ::fix
+   run-clang-tidy.py -checks='-*,moder*over*,read*braces*' >> output.log
+   TidyBackUp.exe
+   echo check and backup check-problem file 
 )
 for /f "delims=" %%a in (BackUp\TidyBackupFile.txt) do ( 
    set listT=%%a
@@ -59,8 +59,8 @@ for /f "delims=" %%a in (BackUp\TidyBackupFile.txt) do (
 echo BackUp Tidy
 
 if %check%==f (
-::ff
-run-clang-tidy.py -checks='-*,moder*over*,read*braces*' -fix >> output.log
+   ::ff
+   run-clang-tidy.py -checks='-*,moder*over*,read*braces*' -fix >> output.log
 )
 
 pause
